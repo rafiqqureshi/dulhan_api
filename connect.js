38872257bea5dfd1aeaa3e9f779fc530e11c18d1
rafiqqureshi <102466436+rafiqqureshi@users.noreply.github.com>
2022-03-27 22:42:23 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,12 +1,5 @@
 var mysql      = require('mysql'); 
-// var connection = mysql.createConnection({
-//   host     : '103.97.88.4',
-//   user     : 'thegloba_jwlr',
-//   password : '?rbjie~t{ce)',
-//   database : 'thegloba_dulhanjawellar',
-//   port: 3306,
-//   debug:true
-// });
+
 var connection = mysql.createConnection({
   host     : 'ftp.jjz.gtx.mybluehost.me',
   user     : 'jjzgtxmy_dulhan',
@@ -16,8 +9,6 @@ var connection = mysql.createConnection({
   debug:true
 });
  
-// connection.connect();
-// console.log("connect")
 connection.connect(function(err) {
   if (err) console.error('error connecting: ' + err.stack)
   else
