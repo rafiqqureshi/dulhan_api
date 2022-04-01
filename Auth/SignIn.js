5b04f1b7f22f0d3c4b2832926fd612ea17e0d165
rafiqqureshi <102466436+rafiqqureshi@users.noreply.github.com>
2022-04-02 00:40:01 +0530

    Update SignIn.js

--- Auth/SignIn.js
+++ Auth/SignIn.js
@@ -7,7 +7,7 @@ var jsonParser = bodyParser.json()
 router.post('/', jsonParser ,function (req, res) {
       var email = req.body.email,
        password = req.body.password;
-       var select =  `SELECT * FROM signup WHERE email='${email}' and password='${password}'`;
+        var select =  `INSERT INTO signup WHERE (email, password) VALUES('${email}','${password}')`;
     connection.query(select, function (error, results, fields) {
       if (error) throw error;
       if(results.length > 0)
